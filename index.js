746bb50904704bf1dfb91eb009ebd82df6e95b65
Rishibakshi <94542074+RishiBakshii@users.noreply.github.com>
2022-06-08 00:35:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// let con=document.querySelector('.con').children[1].addEventListener('mouseenter',function(){
+//     console.log('you lost')
+// })
+// console.log(con)
+
+
+const audio=document.querySelector('audio')
+audio.play()
+
+let con=document.getElementsByName('boxes')
+Array.from(con).forEach(function(element){
+    element.addEventListener('mouseenter',function(){
+        
+        let head=document.querySelector('h1')
+        if(head.innerText==''){
+        head.innerText='You Lost'}
+
+        let head2=document.querySelector('h2')
+        if(head2.innerText==''){
+        head2.innerText='Try Again?'
+        }
+        
+    })
+})
+
+
+let reset=document.querySelector('h2').addEventListener('click',function(){
+    let head=document.querySelector('h1')
+    head.innerText=''
+    let head2=document.querySelector('h2')
+    head2.innerText=''
+})
