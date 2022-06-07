0ebcebbd41f8a13628aec88461a7954a971235c4
Rishibakshi <94542074+RishiBakshii@users.noreply.github.com>
2022-06-08 01:11:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,23 @@
 // console.log(con)
 
 
-let audio=document.querySelector('audio')
 
+let loose=document.querySelector('.loose')
+let audio=new Audio('sound.mp3')
+audio.play()
 let con=document.getElementsByName('boxes')
 Array.from(con).forEach(function(element){
     element.addEventListener('mouseenter',function(){
-        
+        loose.play()
         let head=document.querySelector('h1')
         if(head.innerText==''){
-        head.innerText='You Lost'}
+        head.innerText='You Lost'
+    }
+        audio.pause()
 
         let head2=document.querySelector('h2')
         if(head2.innerText==''){
         head2.innerText='Try Again?'
-        audio.play()
         }
         
     })
@@ -29,4 +32,5 @@ let reset=document.querySelector('h2').addEventListener('click',function(){
     head.innerText=''
     let head2=document.querySelector('h2')
     head2.innerText=''
+    audio.play()
 })
